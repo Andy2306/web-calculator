8aaa31abfe9c751c743cdad9bead397ef5b2cb9a
andy <72691938+Andy2306@users.noreply.github.com>
2025-01-23 02:40:13 +0000

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // fungsi untuk menampilakan nilai kelayar
-function appendValue(value){
-    document.getElementById('display').value += value;
-    console.log("nilai :" + value);
+function appendValue(nilai){
+    document.getElementById('display').value += nilai;
+    console.log("nilai :" + nilai);
 }
-
+/* https://github.com/Andy2306/web-calculator/blob/main/LICENSE */
 // funsi untuk mengkosongkan tampilan layar
 function clearDisplay() {
     document.getElementById('display').value = "";
@@ -20,5 +20,35 @@ function calculate() {
     } catch {
         display.value = 'error';
         console.log("error bjir")
+    }
+}
+
+// fungsi hitung akar pangkat
+function langsungHitungPangkat() {
+    const nilai = document.getElementById('display');
+
+    try {
+        const bilangan = parseFloat(nilai.value);
+        if (bilangan < 0) throw new Error ("tidak dapat menghitung " + bilangan);
+        nilai.value = Math.sqrt(bilangan);
+        console.log("akar kuadrat :" + nilai.value)
+
+    } catch {
+        nilai.value = "error"
+        console.log(nilai.value)
+    }
+}
+
+// fungsi hitung persen (menjadi bilangan sederhana)
+function langsungHitungPersen() {
+    const nilai = document.getElementById('display');
+
+    try {
+        const bilangan = parseFloat(nilai.value);
+        nilai.value = bilangan / 100;
+        console.log("persen :" + nilai.value);
+    } catch {
+        nilai.value = "error"
+        console.log(nilai.value)
     }
 }
